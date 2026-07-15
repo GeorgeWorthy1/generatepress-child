62e11e1f0c31fc0a4e45dfa02c4411811e6b9c68
GeorgeWorthy1 <george.worthy@thecurve.io>
2026-07-15 15:31:40 +0100

    Add automated theme checks

---
+++ stylelint.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: ["stylelint-config-recommended"],
+  ignoreFiles: ["node_modules/**"],
+  rules: {
+    "declaration-block-no-shorthand-property-overrides": null,
+    "no-descending-specificity": null,
+    "no-duplicate-selectors": null,
+    "no-empty-source": null,
+  },
+};
