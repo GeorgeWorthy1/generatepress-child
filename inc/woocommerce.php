94256e494b22c0e6944caddcdb41a881dfa79d6a
GeorgeWorthy1 <george.worthy@thecurve.io>
2026-07-29 10:10:56 +0100

    Release 1.0.11 with cart and content fixes

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -48,6 +48,53 @@ function shs_remove_woocommerce_paged_breadcrumb( $crumbs ) {
 add_filter( 'woocommerce_get_breadcrumb', 'shs_remove_woocommerce_paged_breadcrumb', 20 );
 
 /**
+ * Replace WooCommerce's disabled mini-cart on Cart and Checkout pages.
+ *
+ * WooCommerce intentionally renders the block hidden and disabled on these
+ * pages. A normal cart link keeps the header utility visible and usable while
+ * preserving the existing icon and quantity badge styling.
+ *
+ * @param string $block_content Rendered mini-cart block.
+ * @return string
+ */
+function shs_render_cart_page_header_link( $block_content ) {
+	if ( ! function_exists( 'is_cart' ) || ( ! is_cart() && ! is_checkout() ) ) {
+		return $block_content;
+	}
+
+	$cart_count = 0;
+
+	if ( function_exists( 'WC' ) && WC()->cart ) {
+		$cart_count = WC()->cart->get_cart_contents_count();
+	}
+
+	$cart_label = sprintf(
+		/* translators: %d: Number of products in the cart. */
+		_n( 'Cart, %d item', 'Cart, %d items', $cart_count, 'generatepress-child' ),
+		$cart_count
+	);
+
+	$cart_badge = $cart_count > 0
+		? '<span class="wc-block-mini-cart__badge">' . esc_html( $cart_count ) . '</span>'
+		: '<span class="wc-block-mini-cart__badge"></span>';
+
+	return '<div data-block-name="woocommerce/mini-cart" class="wc-block-mini-cart wp-block-woocommerce-mini-cart shs-static-mini-cart">'
+		. '<a class="wc-block-mini-cart__button shs-static-mini-cart__link" href="' . esc_url( wc_get_cart_url() ) . '" aria-label="' . esc_attr( $cart_label ) . '">'
+		. '<span class="wc-block-mini-cart__quantity-badge">'
+		. '<svg xmlns="http://www.w3.org/2000/svg" class="wc-block-mini-cart__icon" viewBox="0 0 32 32" aria-hidden="true" focusable="false">'
+		. '<circle cx="12.667" cy="24.667" r="2"></circle>'
+		. '<circle cx="23.333" cy="24.667" r="2"></circle>'
+		. '<path fill-rule="evenodd" d="M9.285 10.036a1 1 0 0 1 .776-.37h15.272a1 1 0 0 1 .99 1.142l-1.333 9.333A1 1 0 0 1 24 21H12a1 1 0 0 1-.98-.797L9.083 10.87a1 1 0 0 1 .203-.834m2.005 1.63L12.814 19h10.319l1.047-7.333z" clip-rule="evenodd"></path>'
+		. '<path fill-rule="evenodd" d="M5.667 6.667a1 1 0 0 1 1-1h2.666a1 1 0 0 1 .984.82l.727 4a1 1 0 1 1-1.967.359l-.578-3.18H6.667a1 1 0 0 1-1-1" clip-rule="evenodd"></path>'
+		. '</svg>'
+		. $cart_badge
+		. '</span>'
+		. '</a>'
+		. '</div>';
+}
+add_filter( 'render_block_woocommerce/mini-cart', 'shs_render_cart_page_header_link', 20 );
+
+/**
  * Get an ACF value with a post-meta fallback.
  *
  * @param string $field_name Field name.
@@ -205,4 +252,3 @@ function shs_remove_legacy_acf_product_sections() {
 	}
 }
 add_action( 'wp', 'shs_remove_legacy_acf_product_sections', 20 );
-
